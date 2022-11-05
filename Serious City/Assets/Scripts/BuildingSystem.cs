aecd491206b456076b2bec679a182fd70afcb334
39Nova <79141196+39Nova@users.noreply.github.com>
2022-11-05 22:32:42 +0000

    Updated and fixed building system
    the building system now functions somewhat correctly other than overlapping buildings. A and B to spawn buildings. Space to lock them into position.

--- Serious City/Assets/Scripts/BuildingSystem.cs
+++ Serious City/Assets/Scripts/BuildingSystem.cs
@@ -26,13 +26,50 @@ public class BuildingSystem : MonoBehaviour
 
     #region Unity methods
 
-    private void Awake ()
+    private void Awake()
     {
         current = this;
         grid = gridLayout.gameObject.GetComponent<Grid>();
         // Intialises the current field and gets the grid from the grid layout
     }
 
+<<<<<<< Updated upstream
+=======
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            InitializeWithObject(prefab1);
+        }
+        else if (Input.GetKeyDown(KeyCode.B))
+        {
+            InitializeWithObject(prefab2);
+        }
+        if (!objectToPlace)
+        {
+            return;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (CanBePlaced(objectToPlace))
+            {
+                objectToPlace.Place();
+                Vector3Int start = gridLayout.WorldToCell(objectToPlace.GetStartPosition());
+                TakeArea(start, objectToPlace.Size);
+            }
+            else
+            {
+                Destroy(objectToPlace.gameObject);
+            }
+        }
+        else if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            Destroy(objectToPlace.gameObject);
+        }
+    }
+
+>>>>>>> Stashed changes
     #endregion
 
     #region Utils
@@ -59,5 +96,64 @@ public class BuildingSystem : MonoBehaviour
         return position;
     }
 
+    private static TileBase[] GetTilesBlock(BoundsInt area, Tilemap tilemap)
+    {
+        TileBase[] array = new TileBase[area.size.x * area.size.y * area.size.z];
+        int counter = 0;
+
+        foreach (var v in area.allPositionsWithin)
+        {
+            Vector3Int pos = new Vector3Int(v.x, v.y, 0);
+            array[counter] = tilemap.GetTile(pos);
+            counter++; 
+        }
+
+        return array;
+    }
+
     #endregion
+<<<<<<< Updated upstream
+=======
+
+    #region Building Placement
+
+    public void InitializeWithObject(GameObject prefab)
+    {
+    
+
+    Vector3 position = SnapCoordinateToGrid(Vector3.zero);
+
+    GameObject obj = Instantiate(prefab, position, Quaternion.identity);
+    objectToPlace = obj.GetComponent<PlaceableObject>();
+    obj.AddComponent<ObjectDrag>();
+
+    }
+
+    private bool CanBePlaced(PlaceableObject placeableObject)
+    {
+        BoundsInt area = new BoundsInt();
+        area.position = gridLayout.WorldToCell(objectToPlace.GetStartPosition());
+        area.size = new Vector3Int(area.size.x + 1, area.size.y + 1, area.size.z);
+
+        TileBase[] baseArray = GetTilesBlock(area, MainTilemap);
+
+        foreach (var b in baseArray)
+        {
+            if (b == whiteTile)
+            {
+                return false;
+            }
+        }
+
+        return true; 
+    }
+
+    public void TakeArea(Vector3Int start, Vector3Int size)
+    {
+        MainTilemap.BoxFill(start, whiteTile, startX: start.x, startY: start.y,
+                            start.x + size.x, start.y + size.y);
+    }
+
+    #endregion
+>>>>>>> Stashed changes
 }
--- Serious City/Assets/Scripts/PlaceableObject.cs
+++ Serious City/Assets/Scripts/PlaceableObject.cs
@@ -1,18 +1,58 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class PlaceableObject : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public bool Placed { get; private set; }
+    public Vector3Int Size { get; private set; }
+    private Vector3[] Vertices;
+
+    private void GetColliderVertexPositionLocal()
     {
-        
+        BoxCollider b = gameObject.GetComponent<BoxCollider>();
+        Vertices = new Vector3[4];
+        Vertices[0] = b.center + new Vector3(-b.size.x, -b.size.y, -b.size.z) * 0.5f;
+        Vertices[1] = b.center + new Vector3(b.size.x,-b.size.y, -b.size.z) * 0.5f;
+        Vertices[2] = b.center + new Vector3(b.size.x, -b.size.y, b.size.z) * 0.5f;
+        Vertices[3] = b.center + new Vector3(-b.size.x, -b.size.y, b.size.z) * 0.5f;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void CalculateSizeInCells()
     {
-        
+        Vector3Int[] vertices = new Vector3Int[Vertices.Length];
+
+        for (int i = 0; i < vertices.Length; i++)
+        {
+            Vector3 worldPos = transform.TransformPoint(Vertices[i]);
+            vertices[i] = BuildingSystem.current.gridLayout.WorldToCell(worldPos);
+        }
+
+        Size = new Vector3Int(Math.Abs((vertices[0] - vertices[1]).x), 
+                                Math.Abs((vertices[0] - vertices[3]).y), 
+                                1);
     }
+
+    public Vector3 GetStartPosition()
+    {
+        return transform.TransformPoint(Vertices[0]);
+    }
+
+    private void Start()
+    {
+        GetColliderVertexPositionLocal();
+        CalculateSizeInCells();
+    }
+
+    public virtual void Place()
+    {
+        ObjectDrag drag = gameObject.GetComponent<ObjectDrag>();
+        Destroy(drag);
+
+        Placed = true;
+
+        // This is the place to invoke any Building Events if we need to
+    }
+
 }
