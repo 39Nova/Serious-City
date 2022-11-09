3b8408994337d6a778ce511e42411561277045ff
FelizJueves <jamiedorrens@gmail.com>
2022-11-09 17:23:23 +0000

    Revert "Revert "Pollution Meter Fixed""
    This reverts commit fcdebd071a8222dac23cfeb124d8cf8f2a1323ab.

--- Serious City/Assets/Scripts/PollutionSystem.cs
+++ Serious City/Assets/Scripts/PollutionSystem.cs
@@ -9,12 +9,37 @@ public class PollutionSystem : MonoBehaviour
     public Slider slider;
     public static float PollutionLvl;
     public float PollutionBarLvl;
+    public GameObject PollutionMeter;
+
+    public Sprite Healthy;
+    public Sprite Unsafe;
+    public Sprite Severe;
+    public Sprite Dire;
 
     public void SetPollution(float PollutionLvl)
     {
-        Debug.Log(PollutionLvl);
         PollutionBarLvl = PollutionBarLvl + PollutionLvl;
         slider.value = PollutionBarLvl;
+        if (PollutionBarLvl >=0 && PollutionBarLvl < 25)
+        {
+            Debug.Log("Healthy");
+            PollutionMeter.GetComponent<Image>().sprite = Healthy;
+        }
+        else if (PollutionBarLvl >= 25 && PollutionBarLvl < 50) 
+        {
+            Debug.Log("Unsafe");
+            PollutionMeter.GetComponent<Image>().sprite = Unsafe;
+        }
+        else if (PollutionBarLvl >= 50 && PollutionBarLvl < 75)
+        {
+            Debug.Log("Severe");
+            PollutionMeter.GetComponent<Image>().sprite = Severe;
+        }
+        else if (PollutionBarLvl >= 75 && PollutionBarLvl <= 100)
+        {
+            Debug.Log("Dire");
+            PollutionMeter.GetComponent<Image>().sprite = Dire;
+        }
     }
 
 }
